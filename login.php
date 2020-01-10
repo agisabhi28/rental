7a8bc3a9442331d1f70c0a75dfa4b2a85dd698a2
agisabhi28 <agisabhi28@email.unikom.ac.id>
2020-01-10 21:06:18 +0700

    Tambah Form Login

---
+++ login.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> INSITES 2020 </title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <style>
+      html, body {
+      display: flex;
+      justify-content: center;
+      height: 100%;
+      }
+      body, div, h1, form, input, p {
+      padding: 0;
+      margin: 0;
+      outline: none;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 16px;
+      color: #666;
+      }
+      h1 {
+      padding: 20px 0;
+      font-size: 32px;
+      font-weight: 300;
+      text-align: center;
+      }
+      p {
+      font-size: 12px;
+      text-align: center;
+      opacity: 60%
+      }
+      hr {
+      color: #a9a9a9;
+      opacity: 0.3;
+      }
+      .main-block {
+      max-width: 430px;
+      min-height: 520px;
+      padding: 10px 0;
+      margin: auto;
+      border-radius: 5px;
+      border: solid 1px #ccc;
+      box-shadow: 1px 2px 5px rgba(0,0,0,.31);
+      background: #ebebeb;
+      }
+      form {
+      margin: 0 20px;
+      }
+      input[type=radio] {
+      display: none;
+      }
+      label#icon {
+      margin: 0;
+      border-radius: 5px 0 0 5px;
+      }
+      label.radio {
+      position: relative;
+      display: inline-block;
+      padding-top: 4px;
+      margin-right: 20px;
+      text-indent: 30px;
+      overflow: visible;
+      cursor: pointer;
+      }
+      label.radio:before {
+      content: "";
+      position: absolute;
+      top: 2px;
+      left: 0;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background: #1c87c9;
+      }
+      label.radio:after {
+      content: "";
+      position: absolute;
+      width: 9px;
+      height: 4px;
+      top: 8px;
+      left: 4px;
+      border: 3px solid #fff;
+      border-top: none;
+      border-right: none;
+      transform: rotate(-45deg);
+      opacity: 0;
+      }
+      input[type=radio]:checked + label:after {
+      opacity: 1;
+      }
+      input[type=text], input[type=password] {
+      width: calc(100% - 57px);
+      height: 36px;
+      margin: 13px 0 0 -5px;
+      padding-left: 10px;
+      border-radius: 0 5px 5px 0;
+      border: solid 1px #cbc9c9;
+      box-shadow: 1px 2px 5px rgba(0,0,0,.09);
+      background: #fff;
+      }
+      input[type=password] {
+      margin-bottom: 15px;
+      }
+      #icon {
+      display: inline-block;
+      padding: 9.3px 15px;
+      box-shadow: 1px 2px 5px rgba(0,0,0,.09);
+      background: #1c87c9;
+      color: #fff;
+      text-align: center;
+      }
+      .btn-block {
+      margin-top: 10px;
+      text-align: center;
+      }
+      button {
+      font-family: Tahoma;
+      width: 100%;
+      padding: 20px 0;
+      margin: 15px auto;
+      border-radius: 5px;
+      border: none;
+      background: #1c87c9;
+      font-size: 20px;
+      font-weight: 400;
+      color: #fff;
+      }
+      button:hover {
+      background: #26a9e0;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+  		if (isset($_GET['pesan'])) {
+  			if ($_GET['pesan'] == "gagal") {
+  				echo "<script>alert('Username dan Password Salah')</script>";
+  			}elseif ($_GET['pesan'] == "logout") {
+  				echo "<script>alert('Anda Berhasil Logout')</script>";
+  			}elseif ($_GET['pesan'] == "belum_login") {
+  				echo "Anda Harus Login dulu";
+  			}
+  		}
+  	?>
+    <div class="main-block">
+      <h1>Login Akun</h1>
+      <form action="loginaksi.php" method="post">
+        <hr>
+        <p> <br/><br/><br/> Login USER | SENORITA RENT CAR </p><br/>
+        <label id="icon" for="name"><i class="fas fa-envelope"></i></label>
+        <input type="text" name="username" id="name" placeholder="Email" required/>
+
+        <label id="icon" for="name"><i class="fas fa-unlock-alt"></i></label>
+        <input type="password" name="password" id="name" placeholder="Password" required/>
+        <br/>
+        <hr>
+        <br/>
+        <div class="btn-block">
+          <p> New to INSITES?<a href="pelanggan/register.php"> Register</a>.</p><br/>
+          <button type="submit" value="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
