2550371aa3205f6191c2e837cb96ef01815363b0
agisabhi28 <agisabhi28@email.unikom.ac.id>
2020-01-10 23:18:33 +0700

    daftar user SUCCESSFULL

--- user/daftaruseraksi.php
+++ user/daftaruseraksi.php
@@ -9,19 +9,17 @@ $password	= $_POST['password'];
 $password1  = $_POST['password1'];
 
 if ($_POST['submit']) {
-  $ekstensi_boleh = array('png','jpg');
-  $foto_ktp       = $_FILES['foto_ktp'];
-  $x              = explode('.',$foto_ktp);
-  $ekstensi       = strtolower(end($x));
-  $ukuran         = $_FILES['foto_ktp']['size'];
-  $file_tmp       = $_FILES['foto_ktp']['tmp_name'];
+  $ekstensi_boleh 	= array('png','jpg');
+		$gambar_produk		= $_FILES['foto_ktp']['name'];
+		$x 			= explode('.', $gambar_produk);
+		$ekstensi	= strtolower(end($x));
+		$ukuran		= $_FILES['foto_ktp']['size'];
+		$file_tmp	= $_FILES['foto_ktp']['tmp_name'];
   if($password==$password1){
-
-
-  if (in_array($ekstensi, $ekstensi_boleh) === true) {
-    if ($ukuran < 1044070) {
-      move_upload_file($file_tmp, 'ktp/'.$foto_ktp);
-      $query = mysqli_query($koneksi, "insert into pelanggan values ('','$nama_pelanggan','$pekerjaan','$alamat','$nik','$foto_ktp','$email','$password')")or die(mysqli_error());
+    if(in_array($ekstensi, $ekstensi_boleh) === true){
+        if ($ukuran < 1044070) {
+      move_uploaded_file($file_tmp, '../gambar/'.$gambar_produk);
+      $query = mysqli_query($koneksi, "insert into pelanggan values ('','$nama_pelanggan','$pekerjaan','$alamat','$nik','$gambar_produk','$email','$password')")or die(mysqli_error());
 
     }else {
       echo "<script> alert('Ukuran File Max : 1 MB'); </script>";
@@ -30,13 +28,16 @@ if ($_POST['submit']) {
 
   }else {
     echo "<script> alert('Ekstensi harus JPG atau PNG');</script>";
-    include 'daftaruser.php';
+    include '../index.php';
   }
 
 }else {
-  echo "<script> alert('belum Klik Submit'); </script>";
+  echo "<script> alert('password tidak sama'); </script>";
   include 'daftaruser.php';
 }
+}
+if ($query) {
+  header('location:../index.php?pesan=daftar');
 }
 
 
